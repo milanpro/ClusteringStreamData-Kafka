25631026a78a8781bed676356281cad0827e7d98
Milan Proell <milan.proell@student.hpi.de>
2019-05-14 09:59:06 +0200

    Some not so random generation

--- DataGenerator/src/main/scala/DataGenerator.scala
+++ DataGenerator/src/main/scala/DataGenerator.scala
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.kafka.common.serialization.StringSerializer
 
 import scala.util.Random
+import scala.collection.mutable.ListBuffer
 
 object DataGenerator extends App {
 
@@ -17,8 +18,17 @@ object DataGenerator extends App {
 
   val id = 0
 
+  var distributions: List[((Double, Double),(Double, Double))] = List()
+
+  val distriCount = Random.nextInt(3) + 2
+  for (_ <- 0 to distriCount) {
+    distributions = ((Random.nextDouble()*100, Random.nextDouble()*100),(Random.nextDouble()*100, Random.nextDouble()*100)) :: distributions
+  }
+
   while (true) {
-    val point = Point(Random.nextDouble(), Random.nextDouble())
+    val index = Random.nextInt(distriCount - 1)
+    val distrib = distributions(index)
+    val point = Point(Random.nextGaussian() * distrib._1._1 + distrib._1._2, Random.nextGaussian() * distrib._2._1 + distrib._2._2)
     val record = new ProducerRecord[String, Point]("streams-wordcount-output",s"$id", point)
 
     kafkaProducer.send(record)
