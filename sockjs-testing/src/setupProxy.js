1cc07c68c136147e072e10fab4271b36aaa7711a
Kolya Opahle <kolyaopahle@gmail.com>
2019-07-14 13:15:43 +0200

    added the delay between creating points to etcd

--- sockjs-testing/src/setupProxy.js
+++ sockjs-testing/src/setupProxy.js
@@ -1,6 +1,6 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-  app.use(proxy("/live", { target: "http://localhost:5656" }));
-  app.use(proxy("/live", { target: "ws://localhost:5656", ws: true }));
+  app.use(proxy("/live", { target: "http://35.224.73.235:30136" }));
+  app.use(proxy("/live", { target: "ws://35.224.73.235:30136", ws: true }));
 };
